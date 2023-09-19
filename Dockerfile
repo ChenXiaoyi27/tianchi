00ae576c5a6b50d5adb28e98970f87aab5a85d31
cxy <chenxiaoyi27@163.com>
2023-09-20 01:47:46 +0800

    build报错

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@
 FROM chenxiaoyi27/node:14.21.3
 WORKDIR /app
 COPY package*.json /app/
-RUN npm i --legacy-peer-deps
+RUN npm i
+# --legacy-peer-deps
 # --registry=https://mirrors.cloud.tencent.com/npm/
 COPY . /app
 RUN npm run build
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,9 @@
 // 预览url参数类型声明
 export type PreviewUrlParams = {
-  page_id: string;
-  version_id: string;
+  page_id: string,
+  version_id: string
 };
 // 预览网站url参数类型声明
 export type PreviewSiteUrlParams = {
-  site_id: string;
+  site_id: string
 };
